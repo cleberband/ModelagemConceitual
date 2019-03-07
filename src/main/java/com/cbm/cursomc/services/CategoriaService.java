b09891c5b1fca4934df79a96c36b6372fd6b6258
Cleber Bandeira de Melo <cleberband@gmail.com>
2019-03-07 18:57:33 -0300

    Criando repoository e service para Categoria

---
+++ src/main/java/com/cbm/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.cbm.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cbm.cursomc.domain.Categoria;
+import com.cbm.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
