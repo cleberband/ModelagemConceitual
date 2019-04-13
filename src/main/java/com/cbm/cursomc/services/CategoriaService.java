0d1d814e7d14ae94160b686ebd00929c4eb6e718
Cleber Bandeira de Melo <cleberband@gmail.com>
2019-04-13 14:38:00 -0300

    Validacao sintatica

--- src/main/java/com/cbm/cursomc/services/CategoriaService.java
+++ src/main/java/com/cbm/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.cbm.cursomc.domain.Categoria;
+import com.cbm.cursomc.dto.CategoriaDTO;
 import com.cbm.cursomc.repositories.CategoriaRepository;
 import com.cbm.cursomc.services.exceptions.DataIntegrityException;
 import com.cbm.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
