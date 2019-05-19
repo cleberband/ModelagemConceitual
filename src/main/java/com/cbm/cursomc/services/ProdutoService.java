b6e94c3aa07961469729564a59a1110bbdf1d954
Cleber Bandeira de Melo <cleberband@gmail.com>
2019-05-19 20:34:23 -0300

    Busca produto por categoria

---
+++ src/main/java/com/cbm/cursomc/services/ProdutoService.java
@@ -0,0 +1,39 @@
+package com.cbm.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.cbm.cursomc.domain.Categoria;
+import com.cbm.cursomc.domain.Produto;
+import com.cbm.cursomc.repositories.CategoriaRepository;
+import com.cbm.cursomc.repositories.ProdutoRepository;
+import com.cbm.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository repo;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Produto find(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado com o id: " + id +
+							" Tipo: " + Produto.class.getName()));
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.search(nome, categorias, pageRequest);
+		
+	}
+}
